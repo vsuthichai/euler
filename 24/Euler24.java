4f0459da7367c5c3212f409e6c88227330f34ae8
Victor Suthichai <victor.suthichai@gmail.com>
2013-02-23 16:25:32 -0800

    Problem #24

---
+++ 24/Euler24.java
@@ -0,0 +1,15 @@
+import java.util.*
+
+class Euler24
+{
+  public static void main(String[] args) {
+  	Euler24 permutation = new Euler24();
+
+  }
+
+  private int[] numbers;
+
+  public int[] next() {
+  	
+  }
+}
